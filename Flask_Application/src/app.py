133b51494a7d4814f42499329f83ff42c41f5de2
ajith <ajith1036@gmail.com>
2023-07-04 13:43:05 +0530

    added flask project

--- Flask_Application/src/app.py
+++ Flask_Application/src/app.py
@@ -1,27 +1,18 @@
 from flask import Flask,jsonify,render_template
 import socket
 
+
 app= Flask(__name__)
-
-
+app.debug = True
 def connStatus():
    hostname = socket.gethostname()
    host_ip = socket.gethostbyname(hostname)
-   return str(hostname),str(host_ip)
-
-@app.route('/')
-def hello():
-    return "<h3>Hello world</h3>"
-
-
-@app.route('/health',methods=['GET'])
-def health():
-    return jsonify(
-        status="UP"
-    )   
+   return str(hostname),str(host_ip)  
  
 @app.route('/index',methods=['GET'])
 def template():
     HOSTNAME,IP = connStatus()
     return render_template("index.html",hostname = HOSTNAME,ip = IP  )
 
+if __name__ == '__main__': 
+   app.run()
